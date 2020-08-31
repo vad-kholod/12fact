20ea2718c0e37aa7fceef9ff78b13fd5324f32f6
Vadim Kholod <vad-kholod@mail.ru>
2020-08-31 22:23:09 +0300

    Factor 3. Environment configuration

--- cmd/otus/main.go
+++ cmd/otus/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"os"
+
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	logrus.Info("Hello world")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		logrus.Fatal("Port is not set")
+	}
 }
